4df5919d6924fcdce2bbb8ab22c7654e3481feff
y-monbran <monbran@users.noreply.github.com>
2020-06-09 12:29:41 +0900

    add message-sending

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,29 @@
 class MessagesController < ApplicationController
+  before_action :set_group
+
   def index
-    @messages = Message.all
+    @message = Message.all
+    @messages = @group.messages.includes(:user)
   end
 
   def create
     Message.create(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください'
+      render :index
+    end
   end
 
   private
 
   def message_params
     params.require(:message).permit(:content, :image)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
