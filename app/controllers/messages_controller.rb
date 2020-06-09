741e87e2453cc6a8abf573063e05d795e3362086
y-monbran <monbran@users.noreply.github.com>
2020-06-09 11:08:01 +0900

    add routes&controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,15 @@
 class MessagesController < ApplicationController
   def index
+    @messages = Message.all
+  end
+
+  def create
+    Message.create(message_params)
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image)
   end
 end
