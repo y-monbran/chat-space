794c850beb2adde3af760603cc2ce4e116db9d32
y-monbran <monbran@users.noreply.github.com>
2020-06-09 18:41:19 +0900

    add group.name-group.membername

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   before_action :set_group
 
   def index
-    @message = Message.all
+    @message = Message.new
     @messages = @group.messages.includes(:user)
   end
 
